fc6ee2e26d613c7415af68879395d9dbdcb1695c
Michael <mickeymick25@gmail.com>
2021-05-30 14:01:58 +0200

    updated views, creation of notre fonctionnement page, break code into shared views for hero, footer and nav(s)

---
+++ app/controllers/howitworks_controller.rb
@@ -0,0 +1,5 @@
+class HowitworksController < ApplicationController
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :howitworks
+
   devise_for :users
   root 'home#index'
+
 end
