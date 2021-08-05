597bdf338e5607252730e6964a0d245f27c4a704
blabig <mickaelmoeson@gmail.com>
2021-08-05 18:11:13 +0200

    update Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Type.create(sector: 'immo', catetory: 'features', typeName: 'Ascenseur', typekey: 'lift')
+Type.create(sector: 'immo', catetory: 'features', typeName: 'Gardien', typekey: 'caretaker')
+Type.create(sector: 'immo', catetory: 'features', typeName: 'Balcon', typekey: 'balcony')
+Type.create(sector: 'immo', catetory: 'features', typeName: 'Terrasse', typekey: 'terrace')
+Type.create(sector: 'immo', catetory: 'features', typeName: 'Jardin', typekey: 'garden')
+Type.create(sector: 'immo', catetory: 'features', typeName: 'Piscine', typekey: 'pool')
+Type.create(sector: 'immo', catetory: 'features', typeName: 'Cheminée', typekey: 'fireplace')
+Type.create(sector: 'immo', catetory: 'features', typeName: 'Parquet', typekey: 'woodenfloor')
+Type.create(sector: 'immo', catetory: 'features', typeName: 'Rangement', typekey: 'storage')
+Type.create(sector: 'immo', catetory: 'features', typeName: 'Fibre', typekey: 'fiber')
