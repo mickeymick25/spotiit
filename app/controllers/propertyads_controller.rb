8b8eb4e8a2fa316137baffec83a0eb3b0e672140
blabig <mickaelmoeson@gmail.com>
2021-09-24 00:00:33 +0200

    refonte des annonces de vente (propertyad)

--- app/controllers/propertyads_controller.rb
+++ app/controllers/propertyads_controller.rb
@@ -1,21 +1,48 @@
 class PropertyadsController < ApplicationController
   def index
-    @propertyads = Propertyad.all 
+    @propertyads = Propertyad.includes(classifiedad: [:localisation, :propertyphotos], adfeatures: [:type]).all 
   end
 
   def show
-    @propertyad = Propertyad.find(params[:id])
+    @propertyad = Propertyad.includes(classifiedad: [:localisation,:propertyphotos], adfeatures: [:type]).find(params[:id])
   end
 
   def edit
-    @propertyad = Propertyad.find(params[:id])
+    @propertyad = Propertyad.includes(classifiedad: [:localisation,:propertyphotos], adfeatures: [:type]).find(params[:id])
+    
+    set_featureids
   end
 
   def update
-    @propertyad = Propertyad.find(params[:id])
-    @propertyad.update(classifedad_params)
 
-    redirect_to propertyads_path and return
+    @propertyad = Propertyad.includes(classifiedad: [:localisation,:propertyphotos], adfeatures: [:type]).find(params[:id])
+    
+    @propertyad.classifiedad.assign_attributes(classifiedad_params[:classifiedad_attributes] )
+    
+    @propertyad.classifiedad.valid?    
+
+    if @propertyad.classifiedad.errors.count > 0
+      render 'edit' and return
+    end
+    
+    calcul_reward()
+      
+    self.calcul_reward 
+    
+    if @propertyad.update(all_params)
+        puts "update------------------------------ca a marché"
+        
+        @propertyad.classifiedad.propertyphotos.each do |photo|
+          photo.image_derivatives!
+          photo.save
+        end
+
+        redirect_to propertyads_path and return
+    else
+      
+      set_featureids
+      render 'edit' 
+    end
   end
 
   def destroy
@@ -26,19 +53,108 @@ class PropertyadsController < ApplicationController
   end
 
   def new
+    
+    puts "############### new"
     @propertyad = Propertyad.new
+    
+    @propertyad.classifiedad = Classifiedad.new
+    
+    @propertyad.classifiedad.localisation = Localisation.new
+
+    init_adfeatures @propertyad
+
+    set_featureids
+
+    @insidefeatures = @propertyad.adfeatures. where(type_id: Type.select(:id).where(catetory: 'insidefeatures'))
   end
 
   def create
 
-    propertyad = Propertyad.create(classifedad_params)
+    puts "Create Propertyad ###################################"
+
+    propertyad = Propertyad.new (all_params)
+
+    propertyad.classifiedad.sector = "Immobilier"
+    propertyad.classifiedad.adstatus = "to_validate"
     
-    redirect_to propertyad_path(propertyad.id)
+
+    @propertyad.classifiedad.assign_attributes(classifiedad_params[:classifiedad_attributes] )
+    
+    @propertyad.classifiedad.valid?    
+
+    if @propertyad.classifiedad.errors.count > 0
+      render 'edit' and return
+    end
+    
+    self.calcul_reward()
+
+    if propertyad.save
+        puts "save------------------------------ca a marché"
+        redirect_to propertyads_path and return
+    else
+        puts "save------------------------------ca n'a pas marché"
+        @propertyad = propertyad
+        init_adfeatures (@propertyad)
+        set_featureids
+        render 'new'
+    end
+    
   end
 
   private
-  def classifedad_params
-    params.require(:propertyad).permit(:netprice ,:livingarea, :landarea ,:rooms ,:bedrooms ,:floor ,:buildingyear, :cmtyheating, :energydiagnostic, :carbon, :availability, :description,
-      :orientation, :propertytype,:propertystate)
+
+  def set_featureids
+    @insideids = Type.select(:id).where(catetory: 'insidefeatures').ids
+    @outsideids = Type.select(:id).where(catetory: 'outsidefeatures').ids
+    @sharedids = Type.select(:id).where(catetory: 'sharedfeatures').ids
   end
+
+  def classifiedad_params
+    params.require(:propertyad).permit(classifiedad_attributes: [:title,:short_description,:fixedreward, :sector, :rewardPro, :rewardProPercent,:rewardInd, :rewardIndPercent])
+  end
+
+  def all_params
+    params.require(:propertyad).permit(:id,:netprice ,:livingarea,
+      :landarea ,:rooms ,:bedrooms ,:floor ,:buildingyear, :cmtyheating, :energydiagnostic, :carbon, :availability, 
+      :description, :orientation, :propertytype,:propertystate,
+      classifiedad_attributes: [:id ,:title,:short_description,:fixedreward, :sector, :rewardPro, :rewardProPercent,:rewardInd, :rewardIndPercent, 
+      localisation_attributes: [:id,:number, :street, :district, :city, :dept, :region ], 
+      propertyphotos_attributes: [:id,:title, :type_id, :comment, :image]],
+    adfeatures_attributes: [:id,:selected, :type_id, :comment, :_destroy])
+  end
+
+  def init_adfeatures (propertyad)
+    Type.all.each do |type|
+      if type.catetory == "sharedfeatures"
+        propertyad.adfeatures.new().type = type
+      elsif type.catetory == "insidefeatures"
+        propertyad.adfeatures.new().type = type
+      elsif type.catetory == "outsidefeatures"
+        propertyad.adfeatures.new().type = type
+      end
+    end
+  end
+
+  def calcul_reward ()
+    netprice =  params[:propertyad][:netprice].to_i
+    fixedreward =  params[:propertyad][:classifiedad_attributes][:fixedreward]
+    rewardPro =  params[:propertyad][:classifiedad_attributes][:rewardPro].to_d
+    rewardInd =  params[:propertyad][:classifiedad_attributes][:rewardInd].to_d
+    rewardProPercent =  params[:propertyad][:classifiedad_attributes][:rewardProPercent]
+    rewardIndPercent =  params[:propertyad][:classifiedad_attributes][:rewardIndPercent]
+    
+    if fixedreward == "true"
+      puts fixedreward.inspect
+      params[:propertyad][:classifiedad_attributes][:rewardProPercent] = (rewardPro/netprice*100).round(2)
+      params[:propertyad][:classifiedad_attributes][:rewardIndPercent] = (rewardInd/netprice*100).round(2)
+      puts params[:propertyad][:classifiedad_attributes][:rewardIndPercent].inspect
+    else
+      puts fixedreward.inspect
+      params[:propertyad][:classifiedad][:rewardPro] = (rewardProPercent/100*netprice).round
+      params[:propertyad][:classifiedad][:rewardInd] = (rewardIndPercent/100*netprice).round
+      puts params[:propertyad][:classifiedad][:rewardInd] .inspect
+    end
+
+  end
+
 end
--- app/models/propertyad.rb
+++ app/models/propertyad.rb
@@ -4,9 +4,17 @@ class Propertyad < ApplicationRecord
     enum propertystate: { "Neuf (<5ans)": "recent" , Ancien: "old", Viager: "lifeannuity", "En construciton / VEFA": "offplan", "Non défini": "not_set" }, _suffix: true
     
     belongs_to :classifiedad
-    
-    after_initialize :set_default_property, :if => :new_record?
+    has_many :adfeatures
 
+    accepts_nested_attributes_for :classifiedad, allow_destroy: true 
+    accepts_nested_attributes_for :adfeatures, allow_destroy: true  , reject_if: :adfeatures_invalid
+    
+    validates_associated :classifiedad
+    validates_each :netprice ,:livingarea, :landarea, :rooms, :bedrooms, :buildingyear, :energydiagnostic, :carbon, :availability,
+    :description do |record, attr, value| record.errors.add(attr, '-- le champs ' + attr.to_s + ' est obligatoire') if value.nil? end
+    validates :netprice, :buildingyear,:livingarea, :landarea, numericality: { only_integer: true }
+
+    after_initialize :set_default_property, :if => :new_record?
 
     def set_default_property
         self.propertytype ||= :"Non défini"
@@ -14,5 +22,14 @@ class Propertyad < ApplicationRecord
     end
 
 
+    private
+
+    def adfeatures_invalid(attributes)
+    #   puts " propertywish_invalid #########################"
+      if attributes["id"] == ""
+        # puts attributes["selected"].inspect
+        attributes["selected"] == "0"? true : false
+      end
+    end
 
 end 
