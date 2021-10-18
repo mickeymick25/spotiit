ab9c7daa7ec96dd3892017dddf98fcafe8701d82
blabig <mickaelmoeson@gmail.com>
2021-10-18 14:47:35 +0200

    display wish lvl

--- app/models/propertyadwish.rb
+++ app/models/propertyadwish.rb
@@ -1,5 +1,12 @@
 class Propertyadwish < ApplicationRecord
     
+    enum level: {
+            "Non défini": 0,
+            "Obligatoire": 4,
+            "Souhaité": 3,
+            "Un plus": 2,
+            "Rédhibitoire": 1}
+
     belongs_to :propertybuyad, optional: true
 
     belongs_to :type
